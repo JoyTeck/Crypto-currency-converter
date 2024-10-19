6280afb55edf005a612ca2bff3c00aeab0e16846
JoyTeck <bukunmioloruntobi@gmail.com>
2024-10-19 09:37:22 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const api_key =
-  "4736f40b41950c9e6dafaf033fc6f17fbfd751c5547742d0f4919f8a2e8a59e9";
-const url = "https://min-api.cryptocompare.com/data";
+//const api_key =
+//"4736f40b41950c9e6dafaf033fc6f17fbfd751c5547742d0f4919f8a2e8a59e9";
+// const url = "https://min-api.cryptocompare.com/data";
 
-async function getAllCurrency() {
-  const request = await fetch(`${url}/blockchain/list?api_key=${api_key}`, {
-    mode: "cors",
-  });
+// i really don't know how to go about this api key stuff, still learning how to navigate through it but nevertheless oga freed i go copy your js file input
 
-  const response = await request.json();
-
-  return response;
-}
+const currency = [
+  {
+    symbol: "BTC",
+    name: "Bitcoin",
+  },
+  {
+    symbol: "ETH",
+    name: "Ethereum",
+  },
+  {
+    symbol: "SOL",
+    name: "Solana",
+  },
+];
 
 const select = document.querySelectorAll("select");
-let formattedData = [];
 
-getAllCurrency().then((data) => {
-  console.log(data);
+select.forEach((item) => {
+  currency.forEach((coin) => {
+    const option = document.createElement("options");
+    option.value = coin.name;
+    option.textContent = coin.symbol;
+
+    item.appendChild(option);
+  });
 });
